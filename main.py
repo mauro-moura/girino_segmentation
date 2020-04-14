d0ac1ed78ac4e7ba78623d98d8fe5fb738044f44
Mauro Sérgio <maurosergiostm@gmail.com>
2020-04-14 16:21:42 -0300

    Atualização no polimento do código e correções na normalização

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 
 from unet import unet_completa, dice_coef
-from utils import create_folder, normalize
+from utils import create_folder, normalize, load_images
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from skimage import io
 import glob
 
-data_gen_args = dict(rescale=1./255,
-                    shear_range=0.2,
+data_gen_args = dict(shear_range=0.2,
                     zoom_range=0.2,
                     horizontal_flip=True)
-
 image_datagen = ImageDataGenerator(**data_gen_args)
 
 seed = 1
@@ -20,25 +18,14 @@ size_img = 160
 x_train = sorted(glob.glob('./dados_girino/Train/*'))
 y_train = sorted(glob.glob('./dados_girino/GT/*'))
 
-images = []
-masks = []
-for i in range(len(x_train)):
-    images.append(io.imread(x_train[i]))
-    masks.append(io.imread(y_train[i]))
-
-images = np.asarray(images)
-images = images.reshape(-1, size_img, size_img, 1)
-images = np.float64(images)
-masks = np.asarray(masks)
-masks = masks.reshape(-1, size_img, size_img, 1)
-masks = np.float64(masks)
+images = load_images(x_train)
+masks = load_images(y_train)
 
 image_generator = image_datagen.flow(images, masks,
     batch_size=8,
     seed=seed)
 
 model = unet_completa(size_img, seed)
-
 model.fit_generator(
     image_generator,
     steps_per_epoch=220,
@@ -46,41 +33,27 @@ model.fit_generator(
 
 model.save('girino_test.h5')
 
-images = normalize(images)
-masks = normalize(masks)
-
 print("Carregando novas imagens")
 new_imgs = sorted(glob.glob('./dados_girino/MeanAnisoImJ/*'))
-new_imgs_load = []
-for i in range(len(new_imgs)):
-    new_imgs_load.append(io.imread(new_imgs[i]))
-new_imgs_load = np.asarray(new_imgs_load)
-new_imgs_load = new_imgs_load.reshape(-1, 160, 160, 1)
-new_imgs_load = normalize(new_imgs_load)
+new_imgs_load = load_images(new_imgs)
+
+''' Realizando Novas Predições '''
 new_predicao = model.predict(new_imgs_load)
 new_predicao = new_predicao > 0.5
 new_predicao = np.float64(new_predicao)
-
+create_folder('outputs')
 for i in range(len(new_predicao)):
     io.imsave('./outputs/predicao_%i.png'%(i), new_predicao[i])
 
 print("Calculando o dice para as máscaras conhecidas")
-
 predicao = model.predict(images)
 predicao = predicao > 0.5
 predicao = np.float64(predicao)
-
-sess = tf.InteractiveSession()
 dice_metric = []
-dice_metric.append(dice_coef(predicao, masks).eval())
-sess.close()
-
-print(dice_metric)
-
-create_folder('outputs')
+for i in range(len(predicao)):
+    sess = tf.InteractiveSession()
+    dice_metric.append(dice_coef(predicao[i], masks[i]).eval())
+    sess.close()
+print('Salvando valores de Dice...\nMédia dos Dices: ' + str(np.mean(dice_metric)))
 with open('./outputs/dice_metric.txt', 'w') as file:
     file.write(str(dice_metric))
-
-
-
-
--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
 
 import os
 import numpy as np
+from skimage import io
+import glob
+
+def load_images(img_list, size_img = 160):
+    '''
+    Recebe um glob das imagens e converte em um numpy array no formato que o Keras aceita
+    '''
+    img = []
+    for i in range(len(img_list)):
+        img.append(io.imread(img_list[i]))
+    img = np.asarray(img)
+    img = img.reshape(-1, size_img, size_img, 1)
+    img = normalize(img)
+    return img
 
 def create_folder(dirName):
     # Create target Directory if don't exist
