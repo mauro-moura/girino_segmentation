cb6ee032e3d14966f012d1f1fb63f47ba7ab6616
Mauro Sérgio <maurosergiostm@gmail.com>
2020-09-10 20:06:49 -0300

    Added new augmentation 3D

--- main.py
+++ main.py
@@ -33,14 +33,17 @@ IMG_SAIDA = load_images(y_entrada, size_img = ORIGINAL_SIZE, new_size = NEW_SIZE
 '''
 
 dataAug = MauroDataGenerator()
-IMG_ENTRADA, IMG_SAIDA = dataAug.run_all(x_train, y_train)
+IMG_ENTRADA, IMG_SAIDA = dataAug.run_all(x_train, y_train,
+                                         images, masks,
+                                         size_img=NEW_SIZE,
+                                         Aug_2D=False)
 
 time1 = time.time()
 model = unet_completa(NEW_SIZE, SEED, metric_loss= dice_coef_loss, metric= dice_coef)
 #model = BCDU_net_D3(input_size = (NEW_SIZE, NEW_SIZE, 1), metric_loss= dice_coef_loss, metric= dice_coef)
 #model = vnet(input_size = (20, 200, NEW_SIZE, NEW_SIZE, 1), loss = dice_coef_loss, metrics = [dice_coef])
 
-model.fit(IMG_ENTRADA, IMG_SAIDA, batch_size=20, epochs=50)
+model.fit(IMG_ENTRADA, IMG_SAIDA, batch_size=2, epochs=50)
 
 time2 = time.time()
 TEMPO.append(time2 - time1)
--- modules/data_augmentation.py
+++ modules/data_augmentation.py
@@ -1,10 +1,7 @@
 
-import glob
-from PIL import Image
 import numpy as np
 from skimage import io
 from keras.preprocessing.image import ImageDataGenerator
-
 import matplotlib.pyplot as plt
 
 class MauroDataGenerator():
@@ -25,7 +22,7 @@ class MauroDataGenerator():
         img = img.reshape(-1, img.shape[-2], img.shape[-1], 1)
         return img
 
-    def data_agumentation_v2(self, filenames_x, filenames_y, x, y, datagen,
+    def data_agumentation_2D(self, filenames_x, filenames_y, x, y, datagen,
                         seed = 42, n_img = 8, size_img = 128, save=False,
                         train_path= './Test/Aug_Train', gt_path= './Test/Aug_GT'):
         
@@ -60,15 +57,58 @@ class MauroDataGenerator():
         
         return images, masks
     
-    def run_all(self, filenames_x, filenames_y,
-                    seed = 42, n_img = 8, size_img = 850, save=False,
-                    train_path= './dados_girino/Aug_Train', gt_path= './dados_girino/Aug_GT'):
-        x = self.simple_load_images(filenames_x)
-        y = self.simple_load_images(filenames_y)
+    def data_agumentation_3D(self, filenames_x, filenames_y, x, y, datagen,
+                        seed = 42, n_img = 8, size_img = 128, save=False,
+                        train_path= './Test/Aug_Train', gt_path= './Test/Aug_GT'):
+        
+        images = np.empty((n_img, 0)).tolist()
+        masks = np.empty((n_img, 0)).tolist()
 
-        images, masks = self.data_agumentation_v2(filenames_x, filenames_y, x, y, self.datagen, seed=seed,
-                             n_img=n_img, size_img=size_img, save=save, train_path=train_path,
-                             gt_path=gt_path)
+        for j in range(len(x)):
+            image = x[j].reshape((1, ) + x[j].shape)
+            mask = y[j].reshape((1, ) + y[j].shape)
+
+            i = 0
+            for batch_x in (datagen.flow(image, batch_size= 1, seed=seed)):
+                images[i].append(batch_x.reshape(size_img, size_img, 1))
+                if (save):
+                    FNAME_X = train_path + str(i) + filenames_x[j][8:-4] + '.tiff'
+                    batch_x = batch_x.reshape(size_img, size_img)
+                    plt.imsave(FNAME_X, batch_x, cmap= 'gray', format='tiff')
+                i += 1
+                if i >= n_img:
+                    break
+            
+            i = 0
+            for batch_y in (datagen.flow(mask, batch_size= 1, seed=seed)):
+                masks.insert(j+20*i, batch_y.reshape(size_img, size_img, 1))
+                if (save):
+                    FNAME_Y = gt_path + str(i) + filenames_y[j][5:-4] + '.tiff'
+                    batch_y = batch_y.reshape(size_img, size_img)
+                    plt.imsave(FNAME_Y, batch_y, cmap= 'gray', format='tiff')
+                i += 1
+                if i >= n_img:
+                    break
+        
+        return images, masks
+    
+    def run_all(self, filenames_x, filenames_y, x=np.empty(0), y=np.empty(0),
+                    seed=42, n_img=8, size_img=850, save=False,
+                    train_path='./dados_girino/Aug_Train',
+                    gt_path='./dados_girino/Aug_GT',
+                    Aug_2D=True):
+        if (x.size == 0 and y.size == 0):
+            x = self.simple_load_images(filenames_x)
+            y = self.simple_load_images(filenames_y)
+
+        if (Aug_2D):
+            images, masks = self.data_agumentation_2D(filenames_x, filenames_y, x, y, self.datagen, seed=seed,
+                                 n_img=n_img, size_img=size_img, save=save, train_path=train_path,
+                                 gt_path=gt_path)
+        else:
+            images, masks = self.data_agumentation_3D(filenames_x, filenames_y, x, y, self.datagen, seed=seed,
+                                 n_img=n_img, size_img=size_img, save=save, train_path=train_path,
+                                 gt_path=gt_path)
         
         return np.float64(images), np.float64(masks)
 
